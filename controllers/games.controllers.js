3c08ed72b8f38f693bc5fdd4596064ae35b82644
BrokenRecord85 <pawel_kunicki@hotmail.com>
2022-09-06 10:18:38 +0100

    Finish 4-api-reviews

--- controllers/games.controllers.js
+++ controllers/games.controllers.js
@@ -1,10 +1,21 @@
-const { provideCategories } = require("../models/games.models")
+const { provideCategories, provideReviewsById } = require("../models/games.models")
 
 
 
 exports.getCategories =(req, res, next) => {
     provideCategories().then((categories) => {
         res.send({categories})
+    })
+    .catch(next)
+}
+
+exports.getReviewsById = (req, res, next) => {
+    const review_id = req.params.reviewid
+    provideReviewsById(review_id).then((review) => {
+        if (review === undefined) {
+            res.status(404).send({msg: 'Review not found'})
+        }
+        res.status(200).send({review})
     })
     .catch(next)
 }
--- models/games.models.js
+++ models/games.models.js
@@ -7,3 +7,10 @@ exports.provideCategories = () => {
         return result.rows;
       });
 }
+
+exports.provideReviewsById = (review_id) => {
+  return db.query('SELECT * FROM reviews WHERE review_id=$1', [review_id]).then((result) => {
+    return result.rows[0]
+  })
+
+}
